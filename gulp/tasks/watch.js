c6e7474e20890eeaff8ec382c1dbfed985a5e7a1
Nikita Laitinen <nikitalaitinen@ya.ru>
2018-09-11 13:45:39 +0300

    update watch task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@
 module.exports = function($) {
 	$.gulp.task('watch', function() {
 		$.gulp.watch('./app/js/**/*.js', $.gulp.series('js.process'));
-		$.gulp.watch('./app/template/**/*.pug', $.gulp.series('pug'));
+		$.gulp.watch('./app/template/**/*.pug', $.gulp.series('pug')).on('change', $.browserSync.reload);
 		$.gulp.watch('./app/styles/**/*.scss', $.gulp.series('sass'));
 		$.gulp.watch('./app/imgs/decorate/svg-symbols/*', $.gulp.series('img.symbols.svg'));
 	})
