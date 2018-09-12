5b6309b8a43a6cc6c5d7769ac56dc79d95f35068
Nikita Laitinen <nikitalaitinen@ya.ru>
2018-09-12 13:39:41 +0300

    fix last run bug in img.opt task

--- gulp/tasks/img.opt.js
+++ gulp/tasks/img.opt.js
@@ -3,9 +3,7 @@
 module.exports = function($) {
   $.gulp.task('img.opt', function() {
     return $.combine(
-    	$.gulp.src($.path.imgs.content, {
-    		since: $.gulp.lastRun('img.opt')
-    	}),
+    	$.gulp.src($.path.imgs.content),
     	$.gp.cache($.gp.imagemin({optimizationLevel: 5})),
 		$.gulp.dest($.config.root + '/imgs/content/')
     );
