9836f4e1b657571f2ab9658e3792a475b2b5e7a8
Nikita Laitinen <nikitalaitinen@ya.ru>
2019-01-29 17:39:43 +0300

    add components for js files

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,7 @@
-(function() {
-  'use strict';
+'use strict';
 
-  // console.log('Версия jQuery:',jQuery.fn.jquery);
-  for (let i=0; i<10; i++) {
-    let newccc = [...Array(5)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
-    console.log(newccc);
-  }
-  
+import * as test from './components/test';
+
+(function() {
+  test.init();
 })();
---
+++ app/js/components/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function _init() {
+  console.log('init from module test.js');
+}
+
+export {
+  _init as init
+}
